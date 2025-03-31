3676a1de932c27bf331b18093f6bf27a71b27c9c
Eaby Thomas C <eabythomascu@gmail.com>
2025-03-31 13:00:34 +0530

    added refresh token command

---
+++ core/management/commands/refresh_token.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+from core.services import OAuthServices
+
+class Command(BaseCommand):
+    help = "Fetch a fresh access token using the provided authorization code."
+
+    def handle(self, *args, **kwargs):
+        auth_code = kwargs["auth_code"]
+        
+        try:
+            access_token = OAuthServices.refresh_access_token()
+            self.stdout.write(self.style.SUCCESS(f"Successfully retrieved new access token: {access_token}"))
+        except Exception as e:
+            self.stderr.write(self.style.ERROR(f"Error: {e}"))
