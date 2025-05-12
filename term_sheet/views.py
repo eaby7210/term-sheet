f01601f29d8332e042a953d2db6db667c0f7bfdf
Eaby Thomas C <eabythomascu@gmail.com>
2025-05-12 23:53:36 +0530

    preapprove lookup field fix4

--- term_sheet/views.py
+++ term_sheet/views.py
@@ -241,8 +241,15 @@ class PreApprovalViewSet(viewsets.ModelViewSet):
     lookup_field = "opportunity__ghl_id"
     
     def update(self, request, *args, **kwargs):
-        kwargs['partial'] = True
-        return super().update(request, *args, **kwargs)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
     
     def get_serializer_class(self):
         if hasattr(self, 'action') and self.action in ['list', 'retrieve']:
